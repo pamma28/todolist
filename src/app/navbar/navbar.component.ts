b4d695344feeccc6725a1a554371bc0f1bd15990
Yunas Pamatda email = pamma.cyber@gmail.com <Yunas_P286@mitrais.com>
2020-03-05 11:29:33 +0700

    move logout under navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,7 @@ import { Router } from '@angular/router';
 export class NavbarComponent implements OnInit {
   constructor(private restService: AuthService, private router: Router) {}
   userLoggedIn = false;
+  faPoweOff = faPowerOff;
   ngOnInit() {
     this.userLoggedIn = this.restService.getToken() !== null ? true : false;
     this.restService.obsUserLoggedIn().subscribe(loggedIn => {
