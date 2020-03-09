8db1a5743ad4d3bacdb0c538019ac6e9e8126d20
Yunas Pamatda email = pamma.cyber@gmail.com <Yunas_P286@mitrais.com>
2020-03-09 10:47:12 +0700

    convert navbar into material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
-  imports: [MatButtonModule, MatInputModule, MatFormFieldModule],
-  exports: [MatButtonModule, MatInputModule, MatFormFieldModule],
+  imports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatTabsModule],
+  exports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatTabsModule],
 })
 export class MaterialModule {}
