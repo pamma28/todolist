c9ca34df707b137a09f164833b1a5af9762d2e50
Yunas Pamatda email = pamma.cyber@gmail.com <Yunas_P286@mitrais.com>
2020-03-09 11:35:21 +0700

    add matIcon, change dashboard navbar into mat

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatTabsModule],
-  exports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatTabsModule],
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatIconModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatIconModule,
+  ],
 })
 export class MaterialModule {}
